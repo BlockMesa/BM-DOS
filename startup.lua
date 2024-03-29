a614165d313cf52ee8e5b10d8e66ad8822777936
bronwyn <monstertoast2@gmail.com>
2024-03-29 16:17:45 -0400

    kill default shell

--- startup.lua
+++ startup.lua
@@ -1,8 +1,8 @@
+--inject code stolen from https://pastebin.com/yzfDMjwf
 os.pullEvent = os.pullEventRaw
-term.redirect(term.native)
-term.setPaletteColour(colors.white, 0xE9A226)
-term.setPaletteColour(colors.black, 0x43422C)
+term.redirect(term.native())
 term.clear()
+local oldShell = _G.shell
 if not settings.get("dos.hasFinishedSetup") then
 	settings.set("bios.use_multishell",false)
 	settings.set("shell.allow_disk_startup",false)
@@ -11,14 +11,37 @@ if not settings.get("dos.hasFinishedSetup") then
 	settings.set("dos.hasFinishedSetup",true)
 	settings.save()
 	print("Rebooting...")
+	os.reboot()
 end
-if not fs.find("disk") and not fs.find("/disk/.BOOT") then
-	print("NO BOOT DEVICE FOUND!")
-	while true do os.sleep() end
-else
-	local success, response = pcall(os.run,{['shell']=shell},"/disk/command.com")
-	if not success then
-		print(response)
+local function boot()
+	if not fs.find("disk") and not fs.find("/disk/.BOOT") then
+		print("NO BOOT DEVICE FOUND!")
 		while true do os.sleep() end
+	else
+		local success, response = pcall(os.run,{['shell']=oldShell},"/disk/command.com")
+		if not success then
+			print(response)
+			while true do os.sleep() end
+		end
 	end
 end
+local oldErr = printError
+local function overwrite()
+    _G.printError = oldErr
+    _G['rednet'] = nil
+    os.loadAPI("/rom/apis/rednet")
+	term.redirect(term.native())
+	term.setPaletteColour(colors.white, 0xE9A226)
+	term.setPaletteColour(colors.black, 0x43422C)
+	term.clear()
+	term.setCursorPos(1,1)
+	local success, err = parallel.waitForAny(boot, rednet.run)
+	if not success then
+		print(err)
+		print("Press any key to continue.")
+		os.pullEvent("key")
+	end
+end
+
+_G.printError = inject
+os.queueEvent("terminate")
