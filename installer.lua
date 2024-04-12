deb164b4f79a854e7d2e66245507c2b5270522fc
Bioscreeper <67083372+Bioscreeper@users.noreply.github.com>
2024-04-12 15:04:17 +1000

    Update installer.lua
    Make `Y` the default option

--- installer.lua
+++ installer.lua
@@ -1,20 +1,21 @@
 local baseUrl = "https://raw.githubusercontent.com/BlockMesa/BM-DOS/main/"
 print("Installing BM-DOS")
 if not settings.get("dos.hasFinishedSetup") then
-	term.write("Install BM-DOS to this computer? y/N: ")
+	term.write("Install BM-DOS to this computer? Y/n: ")
 	local command = read()
-	if string.lower(command) == "y" then
-		shell.run("wget https://raw.githubusercontent.com/BlockMesa/BM-BIOS/main/bios.lua startup.lua")
-		shell.run("wget "..baseUrl.."dos/.BOOT .BOOT")
-		shell.run("wget "..baseUrl.."dos/COMMAND.COM COMMAND.COM")
-		shell.run("wget "..baseUrl.."dos/autoexec.bat autoexec.bat")
-
-		--setting the settings
-		settings.set("bios.use_multishell",false)
-		settings.set("shell.allow_disk_startup",false)
-		settings.set("dos.hasFinishedSetup",true)
-		settings.save()
+	if string.lower(command):sub(1,1):lower() == "n" then
+		return
 	end
+	shell.run("wget https://raw.githubusercontent.com/BlockMesa/BM-BIOS/main/bios.lua startup.lua")
+	shell.run("wget "..baseUrl.."dos/.BOOT .BOOT")
+	shell.run("wget "..baseUrl.."dos/COMMAND.COM COMMAND.COM")
+	shell.run("wget "..baseUrl.."dos/autoexec.bat autoexec.bat")
+
+	--setting the settings
+	settings.set("bios.use_multishell",false)
+	settings.set("shell.allow_disk_startup",false)
+	settings.set("dos.hasFinishedSetup",true)
+	settings.save()
 end
 if fs.exists("/disk") then
 	local diskName = peripheral.getName(peripheral.find("drive"))
